ec7a26acaa1bec4a68b39cf310cbd6300d6e3196
hk0305 <hyounkeun2009@gmail.com>
2021-05-25 23:11:16 +0900

    hello-template member test

---
+++ src/main/java/hello/hellospring/SpringCofing.java
@@ -0,0 +1,24 @@
+package hello.hellospring;
+
+//import hello.hellospring.repository.MemberRepository;
+//import hello.hellospring.repository.MemoryMemberRepository;
+//import hello.hellospring.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class SpringCofing {
+    /*
+    자바 코드로 직접 Bean에 등록하기
+     */
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    } 
+//    
+}
