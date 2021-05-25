ee8f2a78539e5d8ba19ec0e53813acce249ec12f
hk0305 <hyounkeun2009@gmail.com>
2021-05-25 15:53:57 +0900

    TDD Sample Commit

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        /* ofNullable
+           null이 넘어올 경우 NPE를 던지지 않고 Optional.empty()와 동일하게 비어 있는 Optional 객체를 얻어옵니다.
+           해당 객체가 null인지 아닌지 알 수 없는 상황에서는 이 메소드를 사용하셔야 합니다.
+         */
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+       return store.values().stream()
+               .filter(member -> member.getName().equals(name))
+               .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+
+    }
+
+    @Test
+    void findMembers() {
+
+    }
+
+    @Test
+    void findOne() {
+
+    }
+
+}
