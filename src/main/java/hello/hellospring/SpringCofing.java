c24a8d6426fd8eb665f06748bbd3b8c9ca61079d
hk0305 <hyounkeun2009@gmail.com>
2021-05-26 00:07:57 +0900

    h2 database connection

--- src/main/java/hello/hellospring/SpringCofing.java
+++ src/main/java/hello/hellospring/SpringCofing.java
@@ -1,24 +1,34 @@
 package hello.hellospring;
 
-//import hello.hellospring.repository.MemberRepository;
-//import hello.hellospring.repository.MemoryMemberRepository;
-//import hello.hellospring.service.MemberService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+import javax.sql.DataSource;
+
+@Configuration
 public class SpringCofing {
-    /*
-    자바 코드로 직접 Bean에 등록하기
-     */
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    } 
-//    
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringCofing(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    /* 자바 코드로 직접 Bean에 등록하기 */
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+
 }
