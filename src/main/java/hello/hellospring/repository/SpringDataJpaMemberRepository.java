7511196101cbcd459cd66a75996675b1dddd8106
hk0305 <hyounkeun2009@gmail.com>
2021-05-30 16:14:42 +0900

    JpaRepository에 대한 주석 추가

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -9,6 +9,9 @@ import java.util.Optional;
 // JpaRepository를 상속하므로 Spring에 자동으로 구현체를 Bean에 등록
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>, MemberRepository {
 
+    // findByName 이라는 메소드명만 사용해야하는 것은 아니다.
+    // findById 등 각 멤버 네임에 맞게 정의하면,
+    // JpaRepository가 알아서 구현체를 만든다.
     @Override
     Optional<Member> findByName(String name);
 
